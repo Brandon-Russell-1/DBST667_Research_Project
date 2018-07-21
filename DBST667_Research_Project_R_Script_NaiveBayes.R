2c24a71634808153358c0da4f7f3a6a9d85d3caf
rbrandon87 <rbrandon87@gmail.com>
2018-07-21 10:26:04 -0400

    Code Cleaning
    Cleaned up the code

--- DBST667_Research_Project_R_Script_NaiveBayes.R
+++ DBST667_Research_Project_R_Script_NaiveBayes.R
@@ -2,19 +2,19 @@
 #Brandon Russell - DBST667
 #Naive Bayes
 #Install and load necessary packages
-#install the e1071 package.  Only once.
 install.packages("e1071")
 install.packages("arules")
 install.packages("caret")
-#load the arules and e1071 library into memory
-#need to do this each time you start the new R session
+install.packages("naivebayes")
+#Load Library files
 library("arules")
 library("e1071")
 library("caret")
 library("klaR")
+library("naivebayes")
 #Verify working directory
 getwd()
-#Load dataset
+#Load dataset - Start here if reloading for fourth model
 bodyfat <- read.csv("fat.csv", header = TRUE, sep = ",")
 #View data to determine if needs any preprocessing
 summary(bodyfat)
@@ -34,7 +34,7 @@ bodyfat$Adiposity.index <- NULL
 
 #Discretize dependent variable
 bodyfat$Percent.body.fat.using.Brozek <- discretize(bodyfat$Percent.body.fat.using.Brozek, "frequency", breaks = 3)
-#Discretize independent variable
+#Discretize independent variable - For fourth model do not perform discretization on independent variables
 bodyfat$Age <- discretize(bodyfat$Age, "frequency", breaks = 3)
 bodyfat$Weight <- discretize(bodyfat$Weight, "frequency", breaks = 3 )
 bodyfat$Height <- discretize(bodyfat$Height, "frequency", breaks = 3 )
@@ -49,49 +49,87 @@ bodyfat$Extended.biceps.circumference <- discretize(bodyfat$Extended.biceps.circ
 bodyfat$Forearm.circumference <- discretize(bodyfat$Forearm.circumference, "frequency", breaks = 3 )
 bodyfat$Wrist.circumference <- discretize(bodyfat$Wrist.circumference, "frequency", breaks = 3)
 
-#make sure that the result is reproducible
+#Split data between training and test sets
 set.seed(1234)
 ind <- sample(2, nrow(bodyfat), replace = TRUE, prob = c(0.7, 0.3))
 train.data <- bodyfat[ind == 1, ]
 test.data <- bodyfat[ind == 2, ]
-
+#From here you can run models one and two or skip to three and four
 myFormula<-Percent.body.fat.using.Brozek~.
-myFormula<-Percent.body.fat.using.Brozek~Height+Chest.circumference+Abdomen.circumference+Wrist.circumference
-#build the model and store in a variable model
+#First Model
 model<-naiveBayes(myFormula, train.data)
 #output the model
 model
 summary(model)
 str(model)
 
-#rpart w/ cross-validation
-my_nb_rpart <- train(myFormula, train.data, method = "naive_bayes",  tuneLength = 100, metric = "Accuracy",
-                     trControl = trainControl(method = "repeatedcv", number = 10, repeats = 5))
-
-my_nb_rpart
-my_nb_rpart$finalModel
-summary(my_nb_rpart)
 #confusion matrix for the training set; need to round the estimated values
-trainpred <- predict(my_nb_rpart, train.data)
+trainpred <- predict(model, train.data)
 summary(trainpred)
 confusionMatrix(trainpred, train.data$Percent.body.fat.using.Brozek)
 
 #confusion matrix for the test data
-testpred <- predict(my_nb_rpart, test.data)
+testpred <- predict(model, test.data)
 summary(testpred)
 confusionMatrix(testpred, test.data$Percent.body.fat.using.Brozek)
-#mosaic plot
-#mosaicplot(table(predict(model, test.data), test.data$Percent.body.fat.using.Brozek), shade=TRUE, main="Predicted vs. Actual")
 
 
 
-actuals_preds <- data.frame(cbind(actuals=test.data$Percent.body.fat.using.Brozek, predicteds=testpred))  # make actuals_predicteds dataframe.
-correlation_accuracy <- cor(actuals_preds) # Return correlation accuracy
+
+#Second model w/ cross-validation
+my_cv_nb <- train(myFormula, train.data, method = "naive_bayes",  tuneLength = 100, metric = "Accuracy",
+                     trControl = trainControl(method = "repeatedcv", number = 10, repeats = 5))
+
+my_cv_nb
+my_cv_nb$finalModel
+summary(my_cv_nb)
+#confusion matrix for the training set; need to round the estimated values
+trainpred <- predict(my_cv_nb, train.data)
+summary(trainpred)
+confusionMatrix(trainpred, train.data$Percent.body.fat.using.Brozek)
+
+#confusion matrix for the test data
+testpred <- predict(my_cv_nb, test.data)
+summary(testpred)
+confusionMatrix(testpred, test.data$Percent.body.fat.using.Brozek)
+
+
+#Third model & Fourth Model - For fourth model go back and only discretize dependent variable
+#Modified formula
+myFormula<-Percent.body.fat.using.Brozek~Height+Chest.circumference+Abdomen.circumference+Wrist.circumference
+#Run the model
+my_cv_nb <- train(myFormula, train.data, method = "naive_bayes",  tuneLength = 100, metric = "Accuracy",
+                  trControl = trainControl(method = "repeatedcv", number = 10, repeats = 5))
+
+my_cv_nb
+my_cv_nb$finalModel
+summary(my_cv_nb)
+#confusion matrix for the training set; need to round the estimated values
+trainpred <- predict(my_cv_nb, train.data)
+summary(trainpred)
+confusionMatrix(trainpred, train.data$Percent.body.fat.using.Brozek)
+
+#confusion matrix for the test data
+testpred <- predict(my_cv_nb, test.data)
+summary(testpred)
+confusionMatrix(testpred, test.data$Percent.body.fat.using.Brozek)
+
+
+
+
+
+
+#Perform accuracy, error, and ROC checks on final model
+# make actuals_predicteds dataframe.
+actuals_preds <- data.frame(cbind(actuals=test.data$Percent.body.fat.using.Brozek, predicteds=testpred))  
+# Return correlation accuracy
+correlation_accuracy <- cor(actuals_preds) 
 correlation_accuracy
 head(actuals_preds)
 
 min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max))  
-min_max_accuracy # Return min max accuracy
+# Return min max accuracy
+min_max_accuracy 
 
 # Calculate error
 error <- actuals_preds$actuals - actuals_preds$predicteds
@@ -104,22 +142,22 @@ mae(error)
 mape(actuals_preds$actuals, actuals_preds$predicteds)
 
 
-#Plot ROC
+#Plot ROC after final model is selected
 lvls = levels(bodyfat$Percent.body.fat.using.Brozek)
-
+#Setup plot field
 aucs = c()
 plot(x=NA, y=NA, xlim=c(0,1), ylim=c(0,1),
      ylab='True Positive Rate',
      xlab='False Positive Rate',
      bty='n')
 legend("bottomright", legend = c(lvls), col = c("red", "green", "blue"), lty=1:2, cex=0.8)
+#Run loop to build ROC for each bin
 for (type.id in 1:3) {
   type = as.factor(train.data$Percent.body.fat.using.Brozek == lvls[type.id])
   
-  my_nb_prediction = predict(model, test.data, type='raw')
+  my_nb_prediction = predict(my_cv_nb, test.data, type='prob')
   
   score = my_nb_prediction[,type.id]
-  #score = my_nb_prediction$posterior[, 'TRUE']
   actual.class = test.data$Percent.body.fat.using.Brozek == lvls[type.id]
   
   nb_pred = prediction(score, actual.class)
@@ -161,6 +199,8 @@ for (type.id in 1:3) {
   browser()
 }
 
+#Reference Line
 lines(x=c(0,1), c(0,1))
+#Mean Area Under Curve
 mean(aucs)
 
--- DBST667_Research_Project_R_Script_Rpart.R
+++ DBST667_Research_Project_R_Script_Rpart.R
@@ -17,7 +17,7 @@ library("ROCR")
 library("klaR")
 #Verify working directory
 getwd()
-#Load dataset
+#Load dataset...start here when reloading data for third, fourth, and fifth model
 bodyfat <- read.csv("fat.csv", header = TRUE, sep = ",")
 #View data to determine if needs any preprocessing
 summary(bodyfat)
@@ -35,6 +35,13 @@ bodyfat$Fat.Free.Weight <- NULL
 bodyfat$Percent.body.fat.using.Siri <- NULL
 bodyfat$Adiposity.index <- NULL
 
+#First Model Discretization
+bodyfat$Percent.body.fat.using.Brozek <- discretize(bodyfat$Percent.body.fat.using.Brozek, "frequency", breaks =6)
+#Perform next line for third model only
+bodyfat$Percent.body.fat.using.Brozek <- discretize(bodyfat$Percent.body.fat.using.Brozek, "frequency", breaks =8)
+#Perform next line for fourth model only
+bodyfat$Percent.body.fat.using.Brozek <- discretize(bodyfat$Percent.body.fat.using.Brozek, "frequency", breaks =4)
+#Perform next line for fifth model only
 bodyfat$Percent.body.fat.using.Brozek <- discretize(bodyfat$Percent.body.fat.using.Brozek, "frequency", breaks =3)
 bodyfat$Percent.body.fat.using.Brozek
 #Split data between training and test sets
@@ -42,55 +49,101 @@ set.seed(1234)
 ind <- sample(2, nrow(bodyfat), replace = TRUE, prob = c(0.7, 0.3))
 train.data <- bodyfat[ind == 1, ]
 test.data <- bodyfat[ind == 2, ]
+#From here you can run models one and two, or skip to three, four, or five
 #Create rpart formula
+#First Model 
 myFormula<-Percent.body.fat.using.Brozek~.
-myFormula<-Percent.body.fat.using.Brozek~Height+Chest.circumference+Abdomen.circumference+Wrist.circumference
-#rpart wo cross validation
+#rpart w/o cross validation
 modFitDecTree <- rpart(myFormula, train.data)
 modFitDecTree
-rpart.plot(modFitDecTree, type = 4, extra = 101)                                       
-#rpart w/ cross-validation
-my_cv_rpart <- train(myFormula, train.data, method = "rpart",  tuneLength = 100, metric = "Accuracy",
-                   trControl = trainControl(method = "repeatedcv", number = 10, repeats = 5))
-
 #Visualize tree
-rpart.plot(my_cv_rpart$finalModel, type = 4, extra = 101)
+rpart.plot(modFitDecTree, type = 4, extra = 101)                                       
 #run predictions and matrix on training data
-pred_rpart <- predict(my_cv_rpart, train.data)
+pred_rpart <- predict(modFitDecTree, train.data, type = "class")
 confusionMatrix(pred_rpart, train.data$Percent.body.fat.using.Brozek)
-
-#Predict on test data
-testpred <- predict(my_cv_rpart, test.data)
+#run predictions and matrix on test data
+testpred <- predict(modFitDecTree, test.data, type = "class")
 confusionMatrix(testpred, test.data$Percent.body.fat.using.Brozek)
 
-actuals_preds <- data.frame(cbind(actuals=test.data$Percent.body.fat.using.Brozek, predicteds=testpred))  # make actuals_predicteds dataframe.
-correlation_accuracy <- cor(actuals_preds) # Return correlation accuracy
+#Second Model
+myFormula<-Percent.body.fat.using.Brozek~Height+Chest.circumference+Abdomen.circumference+Wrist.circumference
+#rpart w/ cross-validation #On second
+my_cv_rpart <- train(myFormula, train.data, method = "rpart",  tuneLength = 50, metric = "Accuracy",
+                     trControl = trainControl(method = "repeatedcv", number = 10))
+#Visualize tree
+rpart.plot(my_cv_rpart$finalModel, type = 4, extra = 101)
+pred_rpart <- predict(my_cv_rpart, train.data)
+#Run on both
+confusionMatrix(pred_rpart, train.data$Percent.body.fat.using.Brozek)
+#Cross-validated model predict
+testpred <- predict(my_cv_rpart, test.data)
+#Run on both
+confusionMatrix(testpred, test.data$Percent.body.fat.using.Brozek)
+
+#Perform Data reload at top with frequency set to 8, then return here.
+#Third Model
+myFormula<-Percent.body.fat.using.Brozek~Height+Chest.circumference+Abdomen.circumference+Wrist.circumference
+#rpart w/ cross-validation #On second
+my_cv_rpart <- train(myFormula, train.data, method = "rpart",  tuneLength = 100, metric = "Accuracy",
+                     trControl = trainControl(method = "repeatedcv", number = 10, repeats = 5))
+#Visualize tree
+rpart.plot(my_cv_rpart$finalModel, type = 4, extra = 101)
+pred_rpart <- predict(my_cv_rpart, train.data)
+#Run on both
+confusionMatrix(pred_rpart, train.data$Percent.body.fat.using.Brozek)
+#Cross-validated model predict
+testpred <- predict(my_cv_rpart, test.data)
+#Run on both
+confusionMatrix(testpred, test.data$Percent.body.fat.using.Brozek)
+
+#Fourth & Fifth Model
+myFormula<-Percent.body.fat.using.Brozek~.
+#rpart w/ cross-validation #On second
+my_cv_rpart <- train(myFormula, train.data, method = "rpart",  tuneLength = 100, metric = "Accuracy",
+                     trControl = trainControl(method = "repeatedcv", number = 10, repeats = 5))
+#Visualize tree
+rpart.plot(my_cv_rpart$finalModel, type = 4, extra = 101)
+pred_rpart <- predict(my_cv_rpart, train.data)
+#Run on both
+confusionMatrix(pred_rpart, train.data$Percent.body.fat.using.Brozek)
+#Cross-validated model predict
+testpred <- predict(my_cv_rpart, test.data)
+#Run on both
+confusionMatrix(testpred, test.data$Percent.body.fat.using.Brozek)
+
+
+
+#Perform accuracy, error, and ROC checks on final model
+
+# make actuals_predicteds dataframe.
+
+actuals_preds <- data.frame(cbind(actuals=test.data$Percent.body.fat.using.Brozek, predicteds=testpred))  
+# Return correlation accuracy
+correlation_accuracy <- cor(actuals_preds) 
 correlation_accuracy
 head(actuals_preds)
 
 min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max))  
-min_max_accuracy # Return min max accuracy
+# Return min max accuracy
+min_max_accuracy 
 
 # Calculate error
 error <- actuals_preds$actuals - actuals_preds$predicteds
-mae <- function(error) {mean(abs(error))}#Returns Mean Absolute Error
-mape <- function(actual,predicted) {mean(abs((predicted - actual))/actual)}#returns Mean Absolute Percentage Error
-rmse <- function(error){sqrt(mean(error^2))} #returns Root Mean Squared Error
 #Run error check functions
 rmse(error)
 mae(error)
 mape(actuals_preds$actuals, actuals_preds$predicteds)
 
-
-#Plot ROC
+#Plot ROC after final model is selected
 lvls = levels(bodyfat$Percent.body.fat.using.Brozek)
-
+#Setup plot field
 aucs = c()
 plot(x=NA, y=NA, xlim=c(0,1), ylim=c(0,1),
      ylab='True Positive Rate',
      xlab='False Positive Rate',
      bty='n')
 legend("bottomright", legend = c(lvls), col = c("red", "green", "blue"), lty=1:2, cex=0.8)
+#Run loop to build ROC for each bin
 for (type.id in 1:3) {
   type = as.factor(train.data$Percent.body.fat.using.Brozek == lvls[type.id])
   
@@ -138,8 +191,9 @@ for (type.id in 1:3) {
   print("-----")
   browser()
 }
-
+#Reference Line
 lines(x=c(0,1), c(0,1))
+#Mean Area Under Curve
 mean(aucs)
 
 
